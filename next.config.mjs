7364c756db764116e65c81c4a058881d2eb4cad2
khanumar03 <khanumar28122003@gmail.com>
2024-10-26 13:12:26 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,11 @@
 const nextConfig = {
     webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
+    config.resolve.fallback = { fs: false, path: false, warnings: false };
     return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
